e745ccc548c493006d9e4ce4d21dbc1fd554c3d9
Asurazzz <2513667783@qq.com>
2022-01-02 12:55:40 +0800

    搭建秒杀项目

---
+++ lagou_service/lagou_service_seckill/src/main/java/com/lagou/seckill/SeckillApplication.java
@@ -0,0 +1,23 @@
+package com.lagou.seckill;
+
+import com.lagou.util.IdWorker;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.lagou.seckill.dao"})
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+}
